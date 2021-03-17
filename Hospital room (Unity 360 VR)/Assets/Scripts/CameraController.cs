9702acfff3e9a153569e0f33c6add6277771d9aa
Nneka Nnagbo <nnekakassandra@gmail.com>
2021-03-16 21:28:42 -0400

    Update CameraController.cs
    Add zoom in/out function

--- Hospital room (Unity 360 VR)/Assets/Scripts/CameraController.cs
+++ Hospital room (Unity 360 VR)/Assets/Scripts/CameraController.cs
@@ -7,6 +7,10 @@ public class CameraController : MonoBehaviour
     //variable for the sensitivity of the mouse
     private float rotateSpeed = 300.0f;
     
+    //zoom in/out variables
+    private float zoomSpeed = 600.0f;
+    private float zoomAmount = 0.0f;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -21,6 +25,13 @@ public class CameraController : MonoBehaviour
             //rotate camera according to the mouse
             transform.localEulerAngles = new Vector3(transform.localEulerAngles.x + Input.GetAxis("Mouse Y") * Time.deltaTime * rotateSpeed, transform.localEulerAngles.y + Input.GetAxis("Mouse X") * Time.deltaTime * rotateSpeed, 0);  //transform is the xyz value of the object 
             //x and y are inverted. z is always going to be 0
+        }
+        
+        if(Input.GetMouseButton(1) || Input.GetMouseButton(2))
+        {
+            //move the camera forward/back
+            zoomAmount = Mathf.Clamp(zoomAmount + Input.GetAxis("Mouse Y") * Time.deltaTime * zoomSpeed, -5.0f, 5.0f);
+            Camera.main.transform.localPosition = new Vector3(0,0, zoomAmount);
         }
     }
 }
